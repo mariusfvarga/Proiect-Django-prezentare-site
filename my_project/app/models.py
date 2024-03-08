4435439a1dd117c5dd0e77e3343ab5f0b19fc2c7
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-03-08 20:00:26 +0200

    Nume in loc de objects

--- my_project/app/models.py
+++ my_project/app/models.py
@@ -9,14 +9,24 @@ class Produs(models.Model):
     descriere = models.CharField(max_length=1024, null=True, blank=True, help_text="Introduceti o descriere" )
     imagine = models.FileField(null=True, blank=True)
     
+    def __str__(self):
+        return f"{self.titlu}"
+    
 class Favorit(models.Model):
     produs = models.ForeignKey(Produs, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.produs}"
     
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     telefon = models.CharField(max_length=15)
     adresa = models.CharField(max_length=100)
     localitate = models.CharField(max_length=20)
-          
+    
+    def __str__(self):
+        return f"{self.user}"
+    
+           
     
