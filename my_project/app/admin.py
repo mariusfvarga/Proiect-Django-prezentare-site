d40219c07c39f9ed8a21bec3c2ce60e4760d8374
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-03-08 19:06:38 +0200

    Adaugare favorite

--- my_project/app/admin.py
+++ my_project/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Produs
+from .models import Produs, Favorit
 
 admin.site.register(Produs)
+admin.site.register(Favorit)
+
--- my_project/app/models.py
+++ my_project/app/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Produs(models.Model):
-    titlu = models.CharField(max_length=50)
+    titlu = models.CharField(max_length=50) #unique=True - daca vrem sa nu avem dubluri la nume
     pret = models.FloatField(db_index = True)
     stoc = models.IntegerField(default=0)
     descriere = models.CharField(max_length=1024, null=True, blank=True, help_text="Introduceti o descriere" )
     imagine = models.FileField(null=True, blank=True)
     
+class Favorit(models.Model):
+    produs = models.ForeignKey(Produs, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
+       
     
