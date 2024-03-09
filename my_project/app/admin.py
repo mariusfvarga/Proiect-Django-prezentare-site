c291a26bdaf146537f5181e9eb4a59aa8d9b81ef
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-03-09 21:56:50 +0200

    Adaugare recenzii

--- my_project/app/admin.py
+++ my_project/app/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Produs, Favorit, UserProfile
+from .models import Produs, Favorit, UserProfile, Recenzie
 
 admin.site.register(Produs)
 admin.site.register(Favorit)
 admin.site.register(UserProfile)
+admin.site.register(Recenzie)
+
 
 
--- my_project/app/models.py
+++ my_project/app/models.py
@@ -12,6 +12,15 @@ class Produs(models.Model):
     def __str__(self):
         return f"{self.titlu}"
     
+class Recenzie(models.Model):
+    produs = models.ForeignKey(Produs, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=1)    
+    titlu = models.CharField(max_length=20)
+    descriere = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"Recenzie {self.produs}"
+    
 class Favorit(models.Model):
     produs = models.ForeignKey(Produs, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
