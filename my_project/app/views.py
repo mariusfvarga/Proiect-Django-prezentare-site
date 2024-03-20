493006382103a8e27c386364b8034c32e397cc9b
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-03-20 17:10:58 +0200

    Use forms to process of the backend

--- my_project/app/views.py
+++ my_project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.mail import send_mail
 
@@ -35,10 +35,13 @@ def produs(request, id):
 def contact(request):
     form = ContactForm()
     if request.method == "POST":
-        email = request.POST["email"]
-        subiect = request.POST["subiect"]
-        mesaj = request.POST["mesaj"]
-        send_mail(subiect, mesaj, from_email="mariusss_1985@yahoo.com", recipient_list=[email])
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subiect = form.cleaned_data["subiect"]
+            mesaj = form.cleaned_data["mesaj"]
+            email = form.cleaned_data["email"]
+            send_mail(subiect, mesaj, from_email="mariusss_1985@yahoo.com", recipient_list=[email])
+            return redirect("/")
     return render(request, "contact.html", {"form": form})
     
  
