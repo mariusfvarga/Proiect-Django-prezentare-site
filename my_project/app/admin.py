1d5cd52217fbe87448dbc44ec0488ded7797dc66
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-04-11 19:12:42 +0300

    admin search, filter

--- my_project/app/admin.py
+++ my_project/app/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Produs, Favorit, UserProfile, Recenzie, Producator
 
-admin.site.register(Produs)
+
 admin.site.register(Favorit)
 admin.site.register(UserProfile)
 admin.site.register(Recenzie)
 admin.site.register(Producator)
 
+class ProdusAdmin(admin.ModelAdmin):
+    search_fields = ("titlu", )
+    list_display = ("titlu", "producator", "stoc", "pret")
+    list_filter = ("producator", )
+
+admin.site.register(Produs, ProdusAdmin)
 
 
