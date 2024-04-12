78082c2d63209ab5ba0e2bc862033230f742b8b2
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-04-12 09:20:17 +0300

    admin list view

--- my_project/app/admin.py
+++ my_project/app/admin.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 
 # Register your models here.
 from .models import Produs, Favorit, UserProfile, Recenzie, Producator
@@ -10,9 +13,14 @@ admin.site.register(Recenzie)
 admin.site.register(Producator)
 
 class ProdusAdmin(admin.ModelAdmin):
-    search_fields = ("titlu", )
+    search_fields = ("titlu", "producator__nume")
     list_display = ("titlu", "producator", "stoc", "pret")
     list_filter = ("producator", )
+    list_per_page = 20
+    list_select_related = ("producator", )
+    list_editable = ("stoc", )
+    
+    
 
 admin.site.register(Produs, ProdusAdmin)
 
