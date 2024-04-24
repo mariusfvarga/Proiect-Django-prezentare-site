5261f17de08b15f2efdb108eb0b0d600b65048ce
Marius Francisc Varga <marius.f.varga@gmail.com>
2024-04-24 19:20:45 +0300

    Retusuri

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -141,7 +141,7 @@ if DEBUG:
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["192.168.65.1", "127.0.0.1", "10.0.2.2"]
     
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 
 MEDIA_ROOT = "galerie"
 
